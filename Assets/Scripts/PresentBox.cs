70aa2f0bb59907911a82bc65f7e8973ca732f361
masahiro-ishigaki <1116006@ed.tus.ac.jp>
2016-12-24 16:44:25 +0900

    santaとpresent

--- Assets/Scripts/PresentBox.cs
+++ Assets/Scripts/PresentBox.cs
@@ -3,14 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PresentBox : MonoBehaviour {
+    /*
+	 * 移動管理。当たり判定はプレイヤーで実装。
+	 * 
+	*/
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    public Vector3 Direction = new Vector3(0, -1, 0);//移動方向
+    public float Speed = 0.1f;//速度
+
+    // Use this for initialization
+    void Start () {
+        Destroy(this.gameObject, 5);
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        this.Direction.Normalize();
+        this.transform.position += this.Direction * this.Speed;
+    }
 }
--- Assets/Scripts/Santa.cs
+++ Assets/Scripts/Santa.cs
@@ -2,13 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Santa : MonoBehaviour {
-
-	public GameObject PresentBox;
+public class Santa : MonoBehaviour
+{
+    public float PresentTimeSpan=0.3f;
+    public GameObject PresentBox;
 
 	// Use this for initialization
 	void Start () {
-		
+	    StartCoroutine(genPresent());
 	}
 	
 	// Update is called once per frame
@@ -17,7 +18,20 @@ public class Santa : MonoBehaviour {
 		this.transform.position = new Vector3(sin,this.transform.position.y,GameManager.Z);
 	}
 
-	public void generatePresent(){//プレゼント生成
+    private IEnumerator genPresent()
+    {
+        // ループ
+        while (true)
+        {
+            // 1秒毎にループします
+            yield return new WaitForSeconds(this.PresentTimeSpan);
+            this.generatePresent();
+            this.PresentTimeSpan = Random.Range(0.5f, 2f);
+        }
+    }
+
+
+    public void generatePresent(){//プレゼント生成
 		Instantiate (this.PresentBox, this.transform.position,Quaternion.identity);
 	}
 
