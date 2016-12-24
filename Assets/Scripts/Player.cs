ef26cbe40cb6352e1ca5052a9d4bd54b82fb2df5
ArLE <arlequin.prx@gmail.com>
2016-12-25 02:01:42 +0900

    add player invincible effect

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,61 +9,81 @@ public class Player: MonoBehaviour {
     public bool IsUnderUmbrella = false;
     public int HasPresents = 0;
 
+    public int IsInvincibleTime = 0;
     public bool IsRun = false;
 
 	// Use this for initialization
 	void Start () {
 		
 	}
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void Update() {
         IsRun = false;
-        if (Input.GetKey("right")) {
-            transform.rotation = Quaternion.Euler(0, 90, 0);
-            if (transform.position.x < GameManager.RightLimit) {
-                transform.position += new Vector3(Speed, 0, 0);
+        AnimatorStateInfo state = GetComponent<Animator>().GetCurrentAnimatorStateInfo(0);
+        if (state.fullPathHash != Animator.StringToHash("Base Layer.Standing_React_Large_From_Back")) {
+            GetComponentInChildren<Renderer>().enabled = true;
+            if (IsInvincibleTime > 0) {
+                IsInvincibleTime -= 1;
+                if (IsInvincibleTime % 10 > 5) {
+                    GetComponentInChildren<Renderer>().enabled = false;
+                }
+                else {
+                    GetComponentInChildren<Renderer>().enabled = true;
+                }
+                
             }
-            IsRun = true;
-        }
-        if (Input.GetKey("left")) {
-            transform.rotation = Quaternion.Euler(0, -90, 0);
-            if (transform.position.x > GameManager.LeftLimit) {
-                transform.position += new Vector3(-Speed, 0, 0);
+
+            if (Input.GetKey("right")) {
+                transform.rotation = Quaternion.Euler(0, 90, 0);
+                if (transform.position.x < GameManager.RightLimit) {
+                    transform.position += new Vector3(Speed, 0, 0);
+                }
+                IsRun = true;
             }
-            IsRun = true;
+            if (Input.GetKey("left")) {
+                transform.rotation = Quaternion.Euler(0, -90, 0);
+                if (transform.position.x > GameManager.LeftLimit) {
+                    transform.position += new Vector3(-Speed, 0, 0);
+                }
+                IsRun = true;
+            }
+
+            if (Input.GetKey("up")) {
+
+            }
         }
+
         GetComponent<Animator>().SetBool("Run", IsRun);
-
-        if (Input.GetKey("up")) {
-
-        }
     }
 
     private void OnTriggerEnter(Collider other) {
-        if (other.tag == "Snow") {
-            Debug.Log("Snow hit.");
-			Destroy (other.gameObject);
-            Life -= 1;
-            GetComponent<Animator>().SetTrigger("Damage");
-            if (Life < 0) {
-                GameManager.Instance.GameFinish();
-                GetComponent<Animator>().SetTrigger("Death");
-                Life = 3;
+        if (IsInvincibleTime == 0) {
+            if (other.tag == "Snow") {
+                Debug.Log("Snow hit.");
+                Destroy(other.gameObject);
+                IsInvincibleTime = 60;
+                Life -= 1;
+                GetComponent<Animator>().SetTrigger("Damage");
+                if (Life < 0) {
+                    GameManager.Instance.GameFinish();
+                    GetComponent<Animator>().SetTrigger("Death");
+                    Life = 3;
+                }
             }
-        }
-        if (other.tag == "PresentBox") {
-			Destroy (other.gameObject);
-            HasPresents += 1;
-        }
-        if (other.tag == "Item") {
-            Debug.Log("Item hit.");
-			var item = other.gameObject.GetComponent<ItemBase> ();
-			item.GetItem (this);
-        }
-        if (other.tag == "Umbrella") {
-            Debug.Log("Umbrella enter.");
-            IsUnderUmbrella = true;
+            if (other.tag == "PresentBox") {
+                Destroy(other.gameObject);
+                HasPresents += 1;
+            }
+            if (other.tag == "Item") {
+                Debug.Log("Item hit.");
+                var item = other.gameObject.GetComponent<ItemBase>();
+                item.GetItem(this);
+            }
+            if (other.tag == "Umbrella") {
+                Debug.Log("Umbrella enter.");
+                IsUnderUmbrella = true;
+            }
         }
     }
     private void OnTriggerExit(Collider other) {
