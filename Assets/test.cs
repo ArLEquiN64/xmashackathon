ca9e758df0b1d69ec4d72e57b8a1af867669deaa
moajo <mimirosiasd@gmail.com>
2016-12-24 11:58:52 +0900

    test

--- Assets/test.cs
+++ Assets/test.cs
@@ -11,6 +11,13 @@ public class test : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		if (Input.GetKey(KeyCode.RightArrow)) {
+			Debug.Log ("右");
+//			transform.position = new Vector3(0f, transform.position.y + 0.1f, 0f);
+		} else if (Input.GetKey(KeyCode.LeftArrow)) {
+			Debug.Log ("左");
+//			transform.position = new Vector3(0f, transform.position.y - 0.1f, 0f);
+		}
 		
 	}
 }
