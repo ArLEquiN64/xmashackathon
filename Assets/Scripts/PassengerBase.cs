7a73c842f0f05d56e4aa7846d5a50614c7d8e518
moajo <mimirosiasd@gmail.com>
2016-12-24 19:22:09 +0900

    add NormalPassanger

--- Assets/Scripts/PassengerBase.cs
+++ Assets/Scripts/PassengerBase.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class PassangerBase : MonoBehaviour {
+public abstract class PassengerBase : MonoBehaviour {
 	public abstract void EnterPlayer ();
 	public abstract void LeavePlayer ();
 }
