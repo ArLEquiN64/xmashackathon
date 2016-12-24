31978c503ba6a9a4384e5586e57c0ebe33bf445e
moajo <mimirosiasd@gmail.com>
2016-12-24 21:21:22 +0900

    add FallObj

---
+++ Assets/Scripts/FallObj.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallObj : MonoBehaviour {
+	
+	public Vector3 Direction=new Vector3(0,-1,0);//移動方向
+	public float Speed=0.05f;//速度
+	public float DestroyTime=10;
+
+	// Use this for initialization
+	public virtual void Start () {
+		Destroy (this.gameObject, this.DestroyTime);
+	}
+
+	// Update is called once per frame
+	public virtual void Update () {
+
+		this.Direction.Normalize ();
+		this.transform.position += this.Direction * this.Speed;
+	}
+}
--- Assets/Scripts/SnowCore.cs
+++ Assets/Scripts/SnowCore.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SnowCore : Snow {
+public class SnowCore : FallObj {
 	private float _d;
 
 	// Use this for initialization
