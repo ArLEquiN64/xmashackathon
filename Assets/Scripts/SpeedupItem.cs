d29bc8f80d272a3a36b252d2db7c3575e859903b
nao-sph <naoki.nagai@icloud.com>
2016-12-24 19:31:45 +0900

    start makeing Item

---
+++ Assets/Scripts/SpeedupItem.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeedupItem : ItemBase {//スピードアップアイテム
+	public override void getItem(Player player){
+		Debug.Log ("TODO:player.Speedを増やして、一定時間後戻す！");
+	}
+
+
+}
