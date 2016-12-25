2f3abf92d030ad0626d7f805285c801797822a5c
nao-sph <naoki.nagai@icloud.com>
2016-12-25 13:41:04 +0900

    plus callPassenger minus speeddown

--- Assets/Scripts/Items/CallPassengerItem.cs
+++ Assets/Scripts/Items/CallPassengerItem.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpeeddownItem : ItemBase {//スピードアップアイテム
+public class CallPassengerItem : ItemBase {//スピードアップアイテム
 	public override void GetItem(Player player){
 		Debug.Log ("TODO:player.Speedを増やして、");
 	}
