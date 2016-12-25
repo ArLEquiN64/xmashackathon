ce4ddf242839a917a22d08aa27ad3bd3f4247a7a
nao-sph <naoki.nagai@icloud.com>
2016-12-25 13:30:48 +0900

    item fixed

--- Assets/Scripts/Items/HeavysnowItem.cs
+++ Assets/Scripts/Items/HeavysnowItem.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpeedupItem : ItemBase {//スピードアップアイテム
+public class HeavysnowItem : ItemBase {//スピードアップアイテム
 	public override void GetItem(Player player){
 		Debug.Log ("TODO:player.Speedを増やして、");
 	}
