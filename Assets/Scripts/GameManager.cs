2c932c7597e008c12ab3800d32eb5603987688eb
moajo <mimirosiasd@gmail.com>
2016-12-25 14:31:19 +0900

    fix

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -50,6 +50,7 @@ public class GameManager : MonoBehaviour {
 		//ゲーム終了
 		SnowGenerator.ClearSnowCores ();
         ItemGenerator.ClearItems();
+		PassengerManager.ClearPassenger ();
 		this.Player.ResetState ();
 		var score = this.PlayTime;
 		this._hiScore = Mathf.Max (this._hiScore, score);
--- Assets/Scripts/PassengerBase.cs
+++ Assets/Scripts/PassengerBase.cs
@@ -8,34 +8,39 @@ public abstract class PassengerBase : MonoBehaviour {
 
 	public float Speed = 0.01f;//速度
 	public GameObject Body;
-	// Use this for initialization
+
+	private float _baseSpeed = 0;
+
 	protected virtual void Start() {
 		GetComponentInChildren<Animator>().SetBool("IsWalk", true);
-		if (this.Speed > 0) {
-			this.Body.transform.rotation = Quaternion.Euler(0, 90, 0);
-		}
-		else {
-			this.Body.transform.rotation = Quaternion.Euler(0, -90, 0);
-		}
 	}
 
 	// Update is called once per frame
 	protected virtual void Update() {
 		if (this.Speed < 0) {
 			this.transform.position += new Vector3(this.Speed, 0, 0);
-		}
-		else {
+			this.Body.transform.rotation = Quaternion.Euler(0, -90, 0);
+		}else {
+			this.Body.transform.rotation = Quaternion.Euler(0, 90, 0);
 			this.transform.position += new Vector3(this.Speed, 0, 0);
 		}
 	}
 
 	public void StopMove() {
 		GetComponentInChildren<Animator>().SetBool("IsWalk", false);
+		this._baseSpeed = this.Speed;
 		this.Speed = 0;
 	}
 
+	public void StartMove() {
+		this.StartMove (this._baseSpeed);
+	}
 	public void StartMove(float speed) {
 		GetComponentInChildren<Animator>().SetBool("IsWalk", true);
 		this.Speed = speed;
+	}
+
+	protected virtual void Destroy(){
+		PassengerManager.PassengerList.Remove (this.gameObject);
 	}
 }
