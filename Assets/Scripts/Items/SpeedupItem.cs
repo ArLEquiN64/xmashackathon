2225dd3b44b3f9709b409b01b6a79d5d36cfb3b8
nao-sph <naoki.nagai@icloud.com>
2016-12-25 15:06:53 +0900

    SpeedUpItem fixed

--- Assets/Scripts/Items/SpeedupItem.cs
+++ Assets/Scripts/Items/SpeedupItem.cs
@@ -7,16 +7,17 @@ using System.Threading;
 public class SpeedupItem : ItemBase {//スピードアップアイテム
 	public LateInvoke Invoker;
 	public float lastingTime = 5; 
+	public float upSpeed = 0.05f;
 
 	public override void GetItem(Player player){
 		Debug.Log ("TODて、klmml;,;l");
 
-		player.Speed += lastingTime;
+		player.Speed += upSpeed;
 
 		var a = Instantiate (Invoker);
-		a.Time = 5f;
+		a.Time = lastingTime;
 		a.Action = () => {
-			player.Speed -= 0.05f;
+			player.Speed -= upSpeed;
 		};
 
 	}
