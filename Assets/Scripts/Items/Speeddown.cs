636c3811124fa00b638906df483d8ffbb863d581
nao-sph <naoki.nagai@icloud.com>
2016-12-25 13:28:51 +0900

    Item added component

---
+++ Assets/Scripts/Items/Speeddown.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeedupItem : ItemBase {//スピードアップアイテム
+	public override void GetItem(Player player){
+		Debug.Log ("TODO:player.Speedを増やして、");
+	}
+
+}
