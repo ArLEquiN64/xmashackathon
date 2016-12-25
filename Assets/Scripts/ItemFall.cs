1b2824a1546879f930ee6ca69faec35800e9ff3d
masahiro-ishigaki <1116006@ed.tus.ac.jp>
2016-12-25 12:20:22 +0900

    item fallの変更のアップ

--- Assets/Scripts/ItemFall.cs
+++ Assets/Scripts/ItemFall.cs
@@ -2,15 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ItemFall : MonoBehaviour {
+public class ItemFall :FallObj {
 
 	// Use this for initialization
 	void Start () {
-		
+		base.Start();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		base.Update();
 	}
+
+    void destroy(){
+        ItemGenerator.Items.Remove(this.gameObject);
+    }
 }
