4c46876d711513130bfd766a05728338b7ebefc2
nao-sph <naoki.nagai@icloud.com>
2016-12-25 12:27:04 +0900

    item update

--- Assets/Scripts/Items/ExtendItem.cs
+++ Assets/Scripts/Items/ExtendItem.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class ExtendItem : ItemBase {//残機が増えるアイテム
 	public override void GetItem(Player player){
+		player.Life += 1;
 		Debug.Log ("TODO:残基を増やす！");
 	}
 
--- Assets/Scripts/Items/SpeedupItem.cs
+++ Assets/Scripts/Items/SpeedupItem.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class SpeedupItem : ItemBase {//スピードアップアイテム
 	public override void GetItem(Player player){
+		player.Speed += 0.05f;
+		new WaitForSeconds (5);
+		player.Speed -= 0.05f;
 		Debug.Log ("TODO:player.Speedを増やして、一定時間後戻す！");
 	}
 
